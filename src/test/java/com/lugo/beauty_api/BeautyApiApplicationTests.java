9dedd09879c8577d34970cee2a6bdf7226a298b1
LugoDv <lugopersonal@gmail.com>
2025-06-27 23:50:32 +0200

    Inicial commit - estructura de Spring Boot

---
+++ src/test/java/com/lugo/beauty_api/BeautyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lugo.beauty_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
